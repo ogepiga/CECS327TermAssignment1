02bda5dab0a894d2b82224096b968080ba26fc1d
Jet Ysalina <jethercy@gmail.com>
2021-04-26 17:55:53 -0700

    Added a header so that multiple messages can be streamed in one connection

--- client.py
+++ client.py
@@ -1,9 +1,29 @@
 import socket
 
+HEADER_SIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
-# s.recv(x) receives information with a buffer size of x bytes
-msg = s.recv(1024)
-# .decode(s) converts the x type bytes into what they were before being sent through bytes()
-print(msg.decode("utf-8"))
+while True:
+    full_msg = ''
+    new_msg = True  # a flag to track if the current message being sent is the start of a new message
+    while True:
+        msg = s.recv(16)
+
+        # if the message is new, the program will get the header which contains information
+        # about the number of characters the msg will have.
+        if new_msg:
+            print(f"New message length: {msg[:HEADER_SIZE]}")
+            msglen = int(msg[:HEADER_SIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        # checks if the message has been fully decoded and prints it.
+        # sets the new_msg flag back to True to be ready for the next message
+        if len(full_msg) - HEADER_SIZE == msglen:
+            print("Full message received:")
+            print(full_msg[HEADER_SIZE:])
+            new_msg = True
+            full_msg = ''
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import socket
+import random
+import time
 
+HEADER_SIZE = 10
 
 # creating socket s
 # family = AF_INET = IPV4
@@ -13,8 +16,21 @@ s.listen(5)
 # listen forever for connections
 while True:
     clientsocket, address = s.accept()  # when a connection is found, store info to variables
-    print(f"Connection from {address} has been established!")
+    print(f"Connection from {address} has been established")
 
-    # data has to be converted into bytes to be sent over the network
-    # bytes() converts anything inside of it into a specific byte, here type is utf-8
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+    # creates a header that will tell the client how many characters of the msg they are receiving.
+    # the header is the first 10 characters of the message with its contents being the len of the message
+    msg = "Welcome to the server" + f" {address}!"
+    msg = f'{len(msg):<{HEADER_SIZE}}' + msg
+
+    # will ask the user what message to send to the client
+    # to show the stream of data to the client
+    while True:
+        msg = input("Please type in something to send to the client: ")
+        msg = f"{len(msg):<{HEADER_SIZE}}" + msg
+
+        print(f"Message sent! length of message: {len(msg)}")
+
+        # data has to be converted into bytes to be sent over the network
+        # bytes() converts anything inside of it into a specific byte, here type is utf-8
+        clientsocket.send(bytes(msg, "utf-8"))
